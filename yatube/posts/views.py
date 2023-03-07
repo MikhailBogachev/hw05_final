a77fc280cbd45255dea4b3870f6e502e0def4a50
Mikhail Bogachev <56482452+MikhailBogachev@users.noreply.github.com>
2023-03-07 15:31:23 +0800

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import (
-    render, get_object_or_404, redirect, get_list_or_404
+    render, get_object_or_404, redirect
 )
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
