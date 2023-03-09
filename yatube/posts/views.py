bfadba1456793a52c46daf7a9ba3b74d78694524
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-03-09 20:51:52 +0800

    review 1.3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -140,9 +140,7 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if author != request.user and (
-        not Follow.objects.filter(user=request.user, author=author).exists()
-    ):
+    if author != request.user:
         Follow.objects.get_or_create(user=request.user, author=author)
     return redirect('posts:follow_index')
 
